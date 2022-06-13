e29d29d464ae2ee4681fa64560c495f268a1ecf3
Phyllis Kodua <phyllis.kodua@gmail.com>
2022-06-13 09:28:36 -0400

    setting up folder and test port

--- phyllis-kodua-brainflix-api/index.js
+++ phyllis-kodua-brainflix-api/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const { PORT, BACKEND_URL } = process.env;
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get('/',(_req,res) => {
+    res.send("Hello world");
+});
+// start the server and listen on port 5000
+app.listen(PORT, () =>{});
